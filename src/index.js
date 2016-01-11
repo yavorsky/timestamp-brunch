cde3b84fb986e6d31edc0b20f4f4a99ecf5d593f
Fabien Lailliau <fabien.lailliau@valtech.fr>
2016-01-11 12:27:02 +0100

    rewrite with es6, version 1.0.0

--- src/index.js
+++ src/index.js
@@ -1,144 +1,228 @@
+'use strict';
 
-var path = require('path');
-var fs = require('fs');
-var recursive = require('recursive-readdir');
-var glob = require('glob');
-var Promise = require('promise');
-var debug = require('debug')('brunch:timestamp');
+const path = require('path');
+const fs = require('fs');
+const recursive = require('recursive-readdir');
+const glob = require('glob');
+const debug = require('debug')('brunch:timestamp');
 
-function TimestampBrunch(brunchCfg){
+const TimestampBrunch = class TimestampBrunch {
 
-    var cfg =  brunchCfg.plugins.timestampbrunch;
+    constructor(brunchCfg) {
 
-    var publicFolder = brunchCfg.paths.public;
+        this.brunchCfg = brunchCfg;
 
-    var timestamp = Math.floor(new Date().getTime()/1000 / 60);
+        this.cfg = brunchCfg.plugins.timestampbrunch;
 
-    this.onCompile = function(generatedFiles){
+        this.publicFolder = brunchCfg.paths.public;
 
+        this.timestamp = Math.floor(new Date().getTime() / 1000 / 60);
+    }
 
+    onCompile(generatedFiles) {
 
-        if(brunchCfg.server.run){
+        if (this.brunchCfg.server.run) {
             debug('Can\'t run with brunch watch');
             return;
         }
 
-        if(brunchCfg.env.length==0){
+        if (this.brunchCfg.env.length == 0) {
             debug('Specify env');
             return;
         }
 
-        if(cfg.environments.indexOf(brunchCfg.env[0])!=-1){
+        if (this.cfg.environments.indexOf(this.brunchCfg.env[0]) != -1) {
 
             debug('Start ');
 
-            this.cleanOld(publicFolder).then(function(f){
+            this.cleanOld(this.publicFolder).then(function (f) {
 
                 debug('Deleted files :: ', f);
 
-                this.renameFile(generatedFiles).then(
-                    function(files){
+                this.renameFilePromised(generatedFiles).then(
+                    function (files) {
 
                         this.replaceContent(files);
 
                     }.bind(this),
 
-                    function(err){
+                    function (err) {
                         throw new Error('Rename file error ', err);
                     }
                 );
 
             }.bind(this));
 
-        }else{
-            debug('Env :: '+cfg.environments+' not found in' , brunchCfg.env);
+        } else {
+            debug('Env :: ' + this.cfg.environments + ' not found in', this.brunchCfg.env);
         }
 
-    };
+    }
 
-    this.renameFile = function(generatedFiles){
+    /*
+     * Get files list
+     * */
+    getReadFile() {
 
+        let publicFolder = this.publicFolder;
 
+        return new Promise(function (resolve, reject) {
 
-        function looping(filePath){
+            return recursive(publicFolder, function (err, files) {
 
-            return new Promise(function(resolve, reject){
+                return resolve(files);
 
-                var dir   = path.dirname(filePath);
-                var ext   = path.extname(filePath);
-                var base  = path.basename(filePath, ext);
+            });
 
-                var currentfile = dir+'/'+base+ext;
+        });
 
-                if(fs.existsSync(currentfile)){
+    }
 
-                    var newName = base+'-'+timestamp+(cfg.suffix ? '.'+cfg.suffix : '')+ext;
+    /*
+     * Clean old generated files
+     * */
+    cleanOld() {
 
-                    fs.rename(currentfile, dir+'/'+newName, function(err){
+        let readFiles = this.getReadFile();
 
-                        if(err) {
-                            throw new Error('rename error ', currentfile, err);
-                        }
+        return readFiles.then(function (files) {
 
+            var promises = [];
 
-                        debug('File renamed  ' +currentfile, ' to ', newName);
+            for (let file in files) {
 
-                        return resolve({
-                            "newName" : newName,
-                            "oldName" : currentfile
-                        });
+                if (/-\d+(\.[^\d]+)?\.[^\d]+$/.test(files[file])) {
 
-                    }.bind(this));
+                    promises.push(
+                        this.removeFiles(files[file])
+                    );
 
-                }else{
-                    debug('File not found ' +files[file]);
-                    throw new Error('rename File not found ', currentfile);
                 }
+            }
 
+            if (promises.length === 0) {
+                return Promise.all(['true']);
+            }
 
-            }.bind(this));
+            return Promise.all(promises);
 
-        }
+        }.bind(this));
+    }
 
-        var promises = [];
+    /*
+     * Unlink files
+     * */
+    removeFiles(file) {
 
-        for (var file in  generatedFiles) {
-            promises.push(looping(generatedFiles[file].path));
+        return new Promise(function (resolve, reject) {
+
+            return fs.unlink(file, function (err) {
+
+                if (err) return reject(err);
+
+                else return resolve(file);
+
+            });
+
+        });
+
+    }
+
+    /*
+     * Rename file promises
+     * */
+    renameFilePromised(generatedFiles) {
+
+        let promises = [];
+
+        let newNameFile = this.timestamp + (this.cfg.suffix ? '.' + this.cfg.suffix : '');
+
+        for (let file in  generatedFiles) {
+
+            promises.push(
+                this.renameFiles(generatedFiles[file].path, newNameFile)
+            );
+
         }
 
         return Promise.all(promises);
 
-    };
+    }
 
-    this.replaceContent = function(filesInfos){
+    /*
+     * Rename file with timestamp and suffix
+     * */
+    renameFiles(filePath, newNameFile) {
+
+        return new Promise(function (resolve, reject) {
+
+            let dir = path.dirname(filePath);
+            let ext = path.extname(filePath);
+            let base = path.basename(filePath, ext);
+
+            let currentfile = dir + '/' + base + ext;
+
+            if (fs.existsSync(currentfile)) {
+
+                let newName = base + '-' + newNameFile + ext;
+
+                fs.rename(currentfile, dir + '/' + newName, function (err) {
+
+                    if (err) {
+                        throw new Error('rename error ', currentfile, err);
+                    }
+
+                    debug('File renamed  ' + currentfile, ' to ', newName);
+
+                    return resolve({
+                        "newName": newName,
+                        "oldName": currentfile
+                    });
+
+                }.bind(this));
+
+            } else {
+                throw new Error('rename File not found ', currentfile);
+            }
+
+        }.bind(this));
+
+
+    }
+
+    /*
+     * Replace sources in referenceFiles with new name
+     *
+     * */
+    replaceContent(filesInfos) {
 
         //parse ref files ex :index.html
-        glob(publicFolder +'/' + cfg.referenceFiles, {}, function (er, files) {
+        glob(this.publicFolder + '/' + this.cfg.referenceFiles, {}, function (er, files) {
 
-            if(er){
+            if (er) {
                 throw new Error('Error with referenceFiles param ', er);
                 return;
             }
 
-            for(var file in files){
+            for (let file in files) {
 
-                if(fs.existsSync(files[file])){
+                if (fs.existsSync(files[file])) {
 
-                    var content = fs.readFileSync(files[file], 'UTF-8');
+                    let content = fs.readFileSync(files[file], 'UTF-8');
 
                     //parse timestamped files
-                    for(var fileInfo in filesInfos){
+                    for (let fileInfo in filesInfos) {
 
-                        var ext   = path.extname(filesInfos[fileInfo].oldName);
-                        var base  = path.basename(filesInfos[fileInfo].oldName, ext);
+                        let ext = path.extname(filesInfos[fileInfo].oldName);
+                        let base = path.basename(filesInfos[fileInfo].oldName, ext);
 
-                        var regExp = new RegExp(base+ext);
+                        let regExp = new RegExp(base + ext);
 
-                        if(regExp.test(content)){
+                        if (regExp.test(content)) {
 
-                            content = content.replace(regExp,filesInfos[fileInfo].newName);
+                            content = content.replace(regExp, filesInfos[fileInfo].newName);
 
-                            debug('Replace in ' + files[file] +' '+ filesInfos[fileInfo].oldName +' by '+ filesInfos[fileInfo].newName);
+                            debug('Replace in ' + files[file] + ' ' + filesInfos[fileInfo].oldName + ' by ' + filesInfos[fileInfo].newName);
 
                         }
 
@@ -146,65 +230,18 @@ function TimestampBrunch(brunchCfg){
 
                     fs.writeFileSync(files[file], content);
 
-                }else{
+                } else {
                     throw new Error('File not found ', files[file]);
                 }
             }
 
         })
 
-    };
-
-    this.cleanOld = function(base, ext){
-
-        var p = new Promise(function(resolve, reject){
-
-            return recursive(publicFolder, function (err, files) {
-
-                return resolve(files);
-
-            })
-        });
-
-        return p.then(function(files){
-
-
-            function unlinkFiles(f){
-
-                return new Promise(function(resolve, reject){
-
-                    return fs.unlink(f, function(err){
-
-                        if(err) return reject(err);
-
-                        else return resolve(f);
-
-                    });
-
-                })
-            }
-
-            var promises = [];
-
-            for(var file in files){
-
-                if(/-\d+(\.[^\d]+)?\.[^\d]+$/.test(files[file])) {
-
-                    promises.push(unlinkFiles(files[file]));
-
-                }
-
-            }
-
-            return Promise.all(promises);
-
-        })
-
-    };
+    }
 }
+;
+
 
 TimestampBrunch.prototype.brunchPlugin = true;
-TimestampBrunch.prototype.type = 'javascript';
-TimestampBrunch.prototype.extension = 'js';
 
 module.exports = TimestampBrunch;
